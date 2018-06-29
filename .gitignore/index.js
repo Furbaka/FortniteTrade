bc16057b79e9c6aa85f6b652ee97e1536490febe
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-06-29 21:55:06 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ var prefix = ("*");
 
 bot.on('ready', () => {
 	bot.user.setGame(bot.guilds.size + " serveurs | *aide");
-	bot.user.setUsername("⚡Rainbow+");
+	bot.user.setUsername("FortniteTrade 🇫🇷");
 });
 
 
