29a519a37a556e35018c2465ef386b816b0b9a22
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-07-01 12:09:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,31 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const request = require('request');
-
+const client = require('discord-rich-presence')('328514801124900866');
+ 
+client.updatePresence({
+  state: 'slithering',
+  details: '🐍',
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now() + 1337,
+  largeImageKey: 'snek_large',
+  smallImageKey: 'snek_small',
+  instance: true,
+});
 var prefix = ("*");
 
 bot.on('ready', () => {
-	bot.user.setGame(bot.guilds.size + " serveurs | *aide");
+	bot.user.setGame("Pour vous servir !");
 	bot.user.setUsername("FortniteTrade 🇫🇷");
+	client.updatePresence({
+  state: 'slithering',
+  details: '🐍',
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now() + 1337,
+  largeImageKey: 'snek_large',
+  smallImageKey: 'snek_small',
+  instance: true,
+});
 });
 
 
@@ -42,6 +61,10 @@ bot.on('message', message => {
 	
     if (message.content.startsWith(prefix + "sondage")) {
 	    
+
+	    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+                return;
+            }
 	message.delete();
 	    
         let args = message.content.split(" ").slice(1);
@@ -60,161 +83,6 @@ bot.on('message', message => {
      });
      }
 
-	
-    if (message.content === prefix + "rigolo"){
-        message.channel.sendMessage("TU ES MOCHE !");
-        console.log("Commande effectué");
-    }
-
-    if (message.content === prefix + "info"){
-        var embed = new Discord.RichEmbed()
-		.setThumbnail(message.guild.iconURL)
-		.setTitle("INFO")
-		.setDescription("Information du serveur")
-		.addField("Nom", message.guild.name)
-		.addField("Membres", message.guild.memberCount)
-		.setColor("0xF4D03F")
-		.setFooter("BOT CRÉÉ PAR MISTIGRIX")
-        message.channel.sendEmbed(embed);
-    
-
-    }
-
-    if (message.content == prefix + "avatar") {
-     // Send the user's avatar URL
-        message.channel.send(message.author.avatarURL);
-    }
-        
-    if (message.content === prefix + "troprigolo"){
-        message.channel.sendMessage("JE T'AIME PAS !");
-        console.log("Commande effectué");
-    }
-    
-    if (message.content.includes(" WAE") || message.content.includes(" wae")){
-	var knuckles = bot.emojis.get("432968588383748116");
-	var lolidragon = bot.emojis.get("433289550375419904");
-	var dab = bot.emojis.get("432915228947120129");
-           var sayings = [`FOLLOW ME MA BRUDDAH I KNOW DA WAE !\n ${knuckles}${knuckles}${knuckles}`,
-										`COME ON MY BRUDAH WE WILL MAKE AN ARMY !\n ${knuckles}${knuckles}${knuckles}${knuckles}${knuckles}${knuckles}`,
-										`DO YOU KNOW DA WAE ! ${knuckles}`,
-		       								`WER IS DA WAE ? ${knuckles}` ,
-										`WER IS DA QUEEN ? ${knuckles}`,
-		       								`THERE IS DA QUEEN !\n${knuckles}${knuckles}${knuckles}${knuckles}${knuckles}${knuckles}${lolidragon}`];
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			message.channel.sendMessage(sayings[result], true);
-    }
-
-    if (message.content.includes(" PATES") || message.content.includes(" BURGER") || message.content.includes(" PIZZA") || message.content.includes(" PÂTES") || message.content.includes(" pates") || message.content.includes(" burger") || message.content.includes(" pizza") || message.content.includes(" pâtes")) {
-        message.reply("Arrête de parler de bouffe j'ai faim :/");
-    }
-    
-    if (message.content.includes(" DAB") || message.content.includes(" dab")){
-	var knuckles = bot.emojis.get("432968588383748116");
-	var lolidragon = bot.emojis.get("433289550375419904");
-	var dab = bot.emojis.get("432915228947120129");
-	var parrot = bot.emojis.get("432970357536718858.")
-           var sayings = [`LE ${dab} NÉ PA MOR !`,
-										`J'EFFECTUE LE ${dab} !`,
-										`J'APPRÉCIE DABBER ${dab} !`,
-		       								`JE FAIS UN ${dab} DU NEZ !`];
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			message.channel.sendMessage(sayings[result], true);
-    }
-    if (message.content == "BOT" || message.content == "bot"){
-        message.channel.sendMessage("Donc... je suis pas un vrai petit garcon ? :/");
-    }
-    
-    if (message.content.includes(" HEURE") || message.content.includes(" heure")) {
-	   message.channel.sendMessage("L'HEURE DE T'ACHETER UNE MONTRE !");
-    }
-    if (message.content.includes("WOW") || message.content.includes("WAW") || message.content.includes("wow") || message.content.includes("waw")){
-        message.reply("T'as vu, c'est dingue !");
-    }
-
-    if (message.content == prefix + "creepy") {
-    	var sayings = ["Mon père m'a souvent mis en garde contre les sosies avant de mourir. Je l'ai vu traverser la rue avec moi.",
-										"Mon père est tombé dans le puits. Je n'étais pas inquiet jusqu'à ce que j'entende des craquements d'os au fond.",
-										"Ce n'était pas à cause du virus que les gens ont commencé à s'entre-dévorer. C'était à cause du goût.",
-		       								"Elle a le coeur sur la main, mais sa cervelle est répandue sur les murs.",
-										"<<Ton ami imaginaire n'est pas réel.>> Quand tu l'as accepté, j'ai commencé à disparaître",
-		       								"Avis à la femme qui n'arrête pas de frapper à ma porte la nuit : Je ne te laisserai pas sortir.",
-										"Mes étudiants m'aiment de tout leur cœur. J'aimerais juste qu'ils ne laissent pas de trace.",
-		       								"<<Maman ! Maman ! Ma poupée parle !>> <<Je sais, ma chérie.>> La voix de Maman venait de la poupée.",
-										"Quelques heures après avoir été enterré vivant, j'ai été soulagé en entendant quelqu'un creuser. Ma joie est vite retombée quand j'ai réalisé que le son venait d'en dessous.",
-										"Remettez les miroirs. Vous regarder nous manque.",
-										"La nuit dernière, j'ai été réveillé par les pleurs d'un enfant. J'ai descendu les escaliers et j'ai fourré un bâillon dans sa bouche.",
-										"Saviez-vous que vous connaissez tous les visages rencontrés durant vos rêves et qu'il est impossible d'en inventer ? Rappelez-vous des créatures et démons de vos cauchemars."];
-
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			message.channel.sendMessage("```fix\n" + sayings[result]+ "\n```");
-    }
-	
-    if (message.content == "!%!DevCommu"){
-        message.guild.channels.find("name", "📰partenariats📰").sendMessage("Bonjour bonsoir @everyone, comment allez vous ? Aujourd'hui j'ai l'immense plaisir de vous présenter le serveur de <@397364192908410880>, un serveur communautaire nommé CodeCommunity_ Celui ci comporte la communauté de la TEAM de développement web, @CodeGroup. En effet, CodeGroup est une équipe de développeurs web, réalisant des projets et apprenant ensemble. Celle-ci comporte plus de 10 membres et recrute d'avantage. Si tu es intéressé par cette fameuse TEAM de développement, je t'invite donc à lire l'annonce pixelsads situé dans le serveur communautaire. Voila, j'ai tout dis :v:\nhttps://discord.gg/m8UvDDE");
-    }
-   
-    if (message.content == "!%!LightLife"){
-        message.channel.sendMessage("__***👾 L↓GHT L↑FE 👾***__ **Recrutement :x:**\n🗺️ CLIQUEZ SUR CE LIEN INCROYABLE POUR COMMENCER L'AVENTURE https://discord.gg/apjU2vb 🗺️\n\n__L↓GHT L↑FE vous de permet de :__\n\n```htm\n📌Rencontrez vos futurs amis et vos futurs rivaux ! | 😄💬 ⇝ 🎭🎭�\n📌Discuter avec plein de gens (pis voir du hentai) ! | 😄💬 ⇝ 😄💬\n📌Jouer sur des jeux avec d'autres joueurs ! | 🎮 ⇝ 🎮⚔️️🎮\n📌Avoir un grade STAR si vous êtes STREAMER/VIDEASTE ! | 📹‍ ⇝ ‍‍‍‍👯‍😎👯‍\n```\n\n🤜 Sur L↓GHT L↑FE vous êtes (PRESQUE) libre de faire votre pub ! | 📰 ⇝ 🤑\n\n👉 ***En plus on a un bot perso qui est très rigolo !*** | 🤖💬 ⇝ 🤣\n\n🔗 CLIQUEZ SUR CE LIEN POUR REJOINDRE LE SERVEUR https://discord.gg/apjU2vb 🔗\n\n\nServeur de <@328514801124900866>\n@everyone");
-    }
-
-    if (message.content === prefix + "invite"){
-        var embed3 = new Discord.RichEmbed()
-		.setThumbnail(bot.user.avatarURL)
-		.setTitle("INVITE")
-		.setDescription("AJOUTER LE BOT A VOTRE SERVEUR !")
-		.addField("Lien", "[Ajoute moi !](https://lightlifefr.wix.com/rainbow)", true)
-		.setColor("0xF4D14F")
-		.setFooter("BOT CRÉÉ PAR MISTIGRIX")
-        message.sendEmbed(embed3);
-    }
-
-    if (message.content.includes("xD") || message.content.includes("lol") || message.content.includes("mdr")) {
-    	var sayings = ["jui mort X)",
-										"ptdr",
-										"C'est pas drôle",
-		       								"C'est pas ouf en faites, jcp pourquoi tu trouve ca drôle",
-										"Ahah",
-		       								"jui mort x)",
-		      								"Tu rigole au pif?",
-										"Attention, vous allez recevoir 100 exemplaires de ***l'humour pour les nuls***",
-										"Juste c'est pas drôle en vrai"];
-
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			message.channel.sendMessage(sayings[result]);
-    }
-	
-    if (message.content == prefix + "blague") {
-	    
-	message.delete();
-	    
-    	var sayings = ["Un homme et un chien sont sur une barque le chien pète, l'homme tombe à l'eau et se noit. Quelle est la race du chien?\n**Un pékinois**",
-										"Tu as 2 poussin t'en veux 1 bas t'en **POUSSIN**",
-										"Qu'est ce qui est jaune est qui attend ?\n**Jonathan**",
-		       								"Quelle est la plus intelligente, la blonde, la rousse ou la brune ?\n**La rousse** parce que c’est un dictionnaire.",
-										"Un fils demande à son père :\n- Papa,c'est quoi la beauté?\n- Tu vois ta mère ?\n- Oui\n- Et ben c'est pas ça!",
-		       								"Un monsieur visite un musée. Soudain il s'arrête et dit au guide :\n- Ah, c'est moche !\n- C'est du Picasso, répond le guide.\nPlus loin, il s'écrie de nouveau :\n- Ah, c'est vraiment moche !\n- Ca Monsieur, c'est un miroir !",
-										"Un jour Dieu dit à Casto de ramer.\nEt depuis, **Castorama**...",
-		       								"2 arabes sont devant une porte, qui sonne ?\nL'alarme.",
-										"Qui a creusé le grand canyon ?\nUn juif qui a perdu 20 centimes.",
-										"Que dit-on a un arabe en costard cravate ?\n- Accusé levez-vous.",
-		       								"Qu'est-ce qu'un arabe à la mer ? \nLa pollution\n\nQu'est-ce que tous les arabes à la mer ?\nLa sollution",
-		       								"Un juif va dans une pizzeria le pizzaiolo lui demande \"Votre pizza vous la préférez cuite au feu de bois ou au gaz ?\"",
-		       								"Les juifs , c'est comme le chaussures y'en a plus en 39 qu'en 45",
-		       								"Pourquoi les petits chinois ne croient-ils pas au Père Noël ?\nParce que ce sont eux qui fabriquent les jouets !",
-										"Quel est le sport préféré des Anglais ?\nLe football.\n\nQuel est le sport préféré des Australiens ?\nLe rugby.\n\nQuel est le sport préféré des Français ?\nLa pétanque.\n\nQuel est le sport préféré des Arabes ?\nTous les sports hippiques.\n**Hippique** tout..."];
-            var result = Math.floor((Math.random() * sayings.length) + 0);
-            
-            var embedX = new Discord.RichEmbed()
-            .setThumbnail(bot.user.avatarURL)
-            .setTitle("😂")
-            .setDescription("Blague pour " + message.author)
-            .addField("Blague", sayings[result], true)
-            .setColor("0xF4D14F")
-	    .setTimestamp();
-    
-            message.channel.sendEmbed(embedX);
-    }
-	
     if (msg.startsWith(prefix + 'SUPPR')) {
 
         async function purge() {
@@ -247,30 +115,15 @@ bot.on('message', message => {
 		.setThumbnail("http://i.imgur.com/9eIhQvf.gif")
 		.setTitle("AIDE")
 		.setDescription("COMMANDES")
-		.addField("FUN", "`*rigolo` | `*troprigolo` | `*creepy` | `*blague`")
-		.addField("UTILES", "`*info` (pour voir les informations du serveurs)\n`*invite` (pour inviter le bot sur votre serv)\n`*avatar` (pour avoir votre PP)\n`*aide` (bah... pour l'aide quoi.)\n`*news` (pour voir mes nouveautées)\n`*sondage <question>` (pour faire un sondage)")
-		.addField("JEUX", "\`*multi <NOM DU JEU>\` (Pour faire une recherche de joueurs)")
-		.addField("PLUS", "`*aide nsfw` pour voir des trucs cochons (**seulement dans un channel nsfw**)", true)
+		.addField("UTILES", "`*INFO` (pour voir les informations du serveurs)\n`*aide` (bah... pour l'aide quoi.)\n`*SONDAGE <question>` (pour faire un sondage)")
+		.addField("FORTNITE", "\`*DUO / *SQUAD\` (Pour faire une recherche de joueurs)\n`*TRADE`")
 		.setColor("0xF4D14F")
-		.setFooter("BOT CRÉÉ PAR MISTIGRIX, qui a caché des easter eggs...")
+		.setFooter("BOT CRÉÉ PAR MISTIGRIX")
         message.channel.sendEmbed(embed2);
     }
-    
-    
-    if (message.content === prefix + "news"){
-        var embed4 = new Discord.RichEmbed()
-		.setThumbnail("http://i.imgur.com/9eIhQvf.gif")
-		.setTitle("VOICI MES NOUVEAUTÉES !")
-		.setDescription("\n")
-		.addField("AJOUTS", "📌Ajout de la commandes : \`*aide nsfw\` Pour voir des trucs cochons (**seulement dans un channel nsfw**)\nAvec en prime des commandes cochones à découvrir avec celle-ci ! :D")
-		.addField("PATCH", "🌀 Des fois je crashais (encore +) 🌀")
-		.setColor("0xF4D14F")
-		.setFooter("Version 1.5")
-        message.channel.sendEmbed(embed4);
-    }	
 	
 	
-    if (msg.startsWith(prefix + "MULTI")) {
+    if (msg.startsWith(prefix + "DUO")) {
 	
 	
 	var multi = bot.emojis.get("434047602602803200");
@@ -280,10 +133,32 @@ bot.on('message', message => {
         let args = message.content.split(" ").slice(1);
         let thingToEcho = args.join(" ");
         var embed = new Discord.RichEmbed()
-            .setTitle("RECHERCHE DE JOUEURS")
+            .setTitle("RECHERCHE D'UN JOUEUR POUR DUO")
 	    .setDescription("Recherche par "+ message.author)
 	    .setThumbnail("https://images.emojiterra.com/emojione/v2/128px/1f579.png")
-            .addField(`Joue à ` + thingToEcho + ` `, `Réagissez avec ${multi} si vous êtes intéressé(e)`, false)
+            .addField(`Réagissez avec ${multi} si vous êtes intéressé(e)`, false)
+            .setColor("0xB40404")
+            .setTimestamp();
+        message.channel.sendEmbed(embed)
+        .then(function (message) {
+        message.react(multi);
+     });
+     }
+
+  if (msg.startsWith(prefix + "SQUAD")) {
+	
+	
+	var multi = bot.emojis.get("434047602602803200");
+	    
+	message.delete();
+	    
+        let args = message.content.split(" ").slice(1);
+        let thingToEcho = args.join(" ");
+        var embed = new Discord.RichEmbed()
+            .setTitle("RECHERCHE DE JOUEURS POUR SQUAD")
+	    .setDescription("Recherche par "+ message.author)
+	    .setThumbnail("https://images.emojiterra.com/emojione/v2/128px/1f579.png")
+            .addField(`Réagissez avec ${multi} si vous êtes intéressé(e)`, false)
             .setColor("0xB40404")
             .setTimestamp();
         message.channel.sendEmbed(embed)
@@ -310,17 +185,6 @@ bot.on('message', message => {
 		message.react(bot.emojis.get("433316429044121601"));
 	}
 	}
-	if (message.content.includes("<@178994276951261184>")) {
-    	var sayings = ["jui mort X)",
-										"Ferarpasser c ouf",
-										"abonné vou",
-		       								"C'est pas ouf en faites, mé aboné vou il ren",
-										"Ahah",
-		       								"OMG Y'a FERAL SUR LE SERV ?! 0O0"];
-
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			message.channel.sendMessage(sayings[result]);
-    }
 	
     if (message.content.startsWith(prefix + "supersondage")) {
 	
@@ -346,19 +210,6 @@ bot.on('message', message => {
      });
      }
 	
-    if (message.content === prefix + "aide nsfw"){
-	if (message.channel.nsfw === false) {
-	return;}
-	    
-        var embed6 = new Discord.RichEmbed()
-		.setThumbnail("http://holly.paheal.net/_images/10f3ed7efc2c3e737b34d755870197b4/2554566%20-%20ELZZombie%20Friendship_is_Magic%20My_Little_Pony%20Rainbow_Dash.png")
-		.setTitle("POUR LES COCHONS 🐷")
-		.setDescription("Vous allez jouir 💦")
-		.addField("Commandes :", "`*nsfw neko` pour avoir des images de petites chattes 😻")
-		.setColor("cc55ee")
-        message.channel.sendEmbed(embed6);
-    }
-	
 if (message.content.startsWith(prefix + "annonce")){
 	 
 	let args = message.content.split(" ").slice(1);
@@ -376,44 +227,6 @@ if (message.content.startsWith(prefix + "annonce")){
 		.setColor("cc55ee")
         message.channel.sendEmbed(embed2);
     }
-
-    if (message.content == prefix + "nsfw neko") {
-	    
-	  
-	    
-	message.delete();
-	
-	if (message.channel.nsfw === false) {
-		return;}
-	   
-    	var image = ["http://metbuat.info/imgs/33db34b34c1420befa0537413d9d27bf.jpg",
-			"https://i.redditmedia.com/Zy0aR4AjdHUoNYFuEo2FKbwxKtRZsuLyB_ZZlULWK4w.jpg?w=543&s=5cba179b12aaf08dab9e97256bd9dc2e",
-			"http://besthentaipics.com/plog-content/images/best-hentai-pictures/hentai-babes/animal-ears-hentai-cat-girl-neko-nude-butt-breasts-tits-anime-hentai.jpg",
-			"https://i.pinimg.com/originals/3f/fa/b9/3ffab95e2b01f6b4c76da3f43a3ca4d7.jpg",
-			"https://pbs.twimg.com/media/BVXoUzfCcAIOBV0.jpg",
-			"https://78.media.tumblr.com/4b529686cefb03172ac811b1599a6820/tumblr_otdcscDWFP1wufel2o1_1280.jpg",
-			"https://78.media.tumblr.com/1e360f38949320c356034f713c538db7/tumblr_ot58zvxbQ61wufel2o1_1280.jpg",
-			"https://78.media.tumblr.com/366dbdf0bbb657d058f3544216f5233e/tumblr_ot5125EYlb1wufel2o1_500.jpg",
-			"https://78.media.tumblr.com/935b597ed9602b8df271afbb0c1c824a/tumblr_ot50i07dwj1wufel2o1_1280.jpg",
-		        "https://i.pinimg.com/originals/52/b7/f0/52b7f01566af91c777c6978b00baac47.jpg",
-			"http://www.koakuma.com.mx/wp-content/uploads/2017/07/ch1.jpg",
-			"https://pm1.narvii.com/6146/da74f23110ff8e73dff1f67fdc05ca3980cabe8f_hq.jpg",
-			"https://i.pinimg.com/originals/a1/a5/86/a1a58684fbcd928c62fd86e3cfbb8930.jpg",
-			"https://pm1.narvii.com/6146/8be3160033d147af98a8e585ad103b679b42a4df_hq.jpg",
-			"https://i.pinimg.com/originals/b6/e3/3d/b6e33d65ab445b328b6a6370ee6899d5.jpg",
-			"https://i.pinimg.com/originals/08/19/8b/08198bb619e8a189c4b8c5ac960b4c0b.jpg",
-			"https://i.pinimg.com/originals/f3/1e/36/f31e366a4e3866b623c4b3a9be5394b9.jpg",
-			"https://78.media.tumblr.com/aa805ec86b74ff1fab5725a38bd674b4/tumblr_ot49n6DWku1wufel2o1_1280.jpg"];
-            var result = Math.floor((Math.random() * image.length) + 0);
-            
-            var embedX = new Discord.RichEmbed()
-	    .setImage(image[result])
-            .setColor("0xF4D14F")
-	    .setFooter("pour " + message.author.username , message.author.avatarURL)
-    
-            message.channel.sendEmbed(embedX);
-    }
-
  	if (msg.startsWith("GG ")){
 		var nam = message.mentions.members.first();
 		if (message.content == "GG " + nam){
